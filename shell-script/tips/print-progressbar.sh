92fc71cc37c375ceeeae192dd28f69027c7bfce8
Taeik Lim <sibera21@gmail.com>
2019-10-12 15:56:06 +0900

    play

--- shell-script/tips/print-progressbar.sh
+++ shell-script/tips/print-progressbar.sh
@@ -9,7 +9,7 @@ while [ -h "$SOURCE" ]; do
   # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
   [[ $SOURCE != /* ]] && SOURCE="$SCRIPT_HOME/$SOURCE"
 done
-SCRIPT_HOME="$( cd -P "$( dirname "$SOURCE" )" >/dev/null && pwd )"
+readonly SCRIPT_HOME="$( cd -P "$( dirname "$SOURCE" )" >/dev/null && pwd )"
 
 # now $SCRIPT_HOME holds absolute path of this script
 echo "Script home: $SCRIPT_HOME"
