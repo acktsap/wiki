c15ecb2f89514542d9bfcf651e7915403d432e6b
Taeik Lim <sibera21@gmail.com>
2019-10-27 14:48:53 +0900

    play

--- java/spring-rest-server/rest-main/src/main/java/acktsap/sample/RestServerLauncher.java
+++ java/spring-rest-server/rest-main/src/main/java/acktsap/sample/RestServerLauncher.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestApplication {
+public class RestServerLauncher {
 
   public static void main(String[] args) {
-    SpringApplication.run(RestApplication.class, args);
+    SpringApplication.run(RestServerLauncher.class, args);
   }
 
 }
