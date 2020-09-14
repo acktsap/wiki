44c4bc383494f737aa55bbe2981e692ab1186718
Taeik Lim <sibera21@gmail.com>
2020-09-15 02:20:13 +0900

    play
    Signed-off-by: Taeik Lim <sibera21@gmail.com>

--- productivity/build-tool/gradle/basic/build.gradle.kts
+++ productivity/build-tool/gradle/basic/build.gradle.kts
@@ -4,6 +4,9 @@
     https://docs.gradle.org/current/userguide/tutorial_using_tasks.html#learning-the-basics
 */
 
+
+
+
 /**************************************
  * Basic
  **************************************/
@@ -14,6 +17,7 @@
         Each project is made up of one or more tasks
 */
 
+
 /* 
     Hello world task
         ./gradlew hello
@@ -23,6 +27,7 @@ tasks.register("hello") {
         println("Hello world!")
     }
 }
+
 
 /*
     Build scripts are code
@@ -38,6 +43,7 @@ tasks.register("upper") {
     }
 }
 
+
 /*
     Task dependencies
         ./gradlew taskX
@@ -49,11 +55,13 @@ tasks.register("taskX") {
         println("taskX")
     }
 }
+
 tasks.register("taskY") {
     doLast {
         println("taskY")
     }
 }
+
 
 /*
     Dynamic tasks
@@ -66,6 +74,7 @@ repeat(4) { counter ->
         }
     }
 }
+
 
 /*
     Manipulating existing tasks
@@ -96,6 +105,7 @@ hey {
     }
 }
 
+
 /*
     Extra task properties
         ./gradlew printProp
@@ -103,20 +113,26 @@ hey {
 tasks.register("myTask") {
     extra["myProperty"] = "myValue"
 }
+
 tasks.register("printProp") {
     doLast {
         println(tasks["myTask"].extra["myProperty"])
     }
 }
 
+
 /*
     Using methods
         ./gradlew method
 */
 tasks.register("method") {
-    methodTest("Hello method!")
+    doLast {
+        methodTest("Hello method!")
+    }
 }
+
 fun methodTest(message: String): Unit = println(message)
+
 
 /*
     Default tasks
@@ -129,11 +145,13 @@ task("clean") {
         println("Default Cleaning!")
     }
 }
+
 tasks.register("run") {
     doLast {
         println("Default Running!")
     }
 }
+
 
 /*
     Configuration phase hook
@@ -145,12 +163,14 @@ tasks.register("distribution") {
         println("We build the zip with version=$version")
     }
 }
+
 tasks.register("release") {
     dependsOn("distribution")
     doLast {
         println("We release now")
     }
 }
+
 // hook
 gradle.taskGraph.whenReady {
     // version must be read lazily
@@ -161,8 +181,168 @@ gradle.taskGraph.whenReady {
 
 
 
+
 /**************************************
  * Task Details
  **************************************/
 
- TODO
+/*
+    Gradle tasks can has their own properties and method 
+
+    Gradle tasks outcomes
+        (no label) or EXECUTED
+        UP-TO-DATE : By Incremental Builds
+        FROM-CACHE : Outputs from a previous execution
+        SKIPPED : eg. onlyIf returns false
+        NO-SOURCE : eg. No *.java file to compile
+*/
+
+
+/*
+    Defining tasks
+        ./gradlew copy1 or copy2 or copy3
+*/
+tasks.register<Copy>("copy1") {
+    from(file("srcDir"))
+    into(buildDir)
+    doLast {
+        println("copy1")
+    }
+}
+
+tasks {
+    register<Copy>("copy2") {
+        from(file("srcDir"))
+        into(buildDir)
+        doLast {
+            println("copy2")
+        }
+    }
+}
+
+val copy3 by tasks.registering(Copy::class) {
+    from(file("srcDir"))
+    into(buildDir)
+    doLast {
+        println("copy3")
+    }
+}
+
+/*
+    Locating tasks
+        ./gradlew access
+        ./gradlew needCopy
+*/
+task("accessTarget")
+
+tasks.register("access") {
+    doLast {
+        // using a DSL specific syntax
+        val accessTarget by tasks.getting
+        println("Access by dsl - ${accessTarget.name}")
+
+        // via tasks collection
+        println("Access by tasks collection1 - ${tasks["accessTarget"].name}")
+        println("Access by tasks collection2 - ${tasks.named("accessTarget").get().name}")
+        
+        // by path
+        println("Access by path1 - ${tasks.getByPath("accessTarget").path}")
+        println("Access by path1 - ${tasks.getByPath(":accessTarget").path}")
+    }
+}
+
+// by type
+tasks.register("needCopy") {
+    dependsOn(tasks.withType<Copy>())
+}
+
+
+/*
+    Configuring tasks
+        ./gradlew access
+        ./gradlew needCopy
+*/
+
+tasks.register("myConfig")
+
+// configure using Kotlin delegated properties and a lambda
+val myConfig = tasks.named("myConfig") {
+    doLast {
+        println("My config1")
+    }
+}
+
+myConfig {
+    doLast {
+        println("My config2")
+    }
+}
+
+
+/*
+    Passing arguments to a task constructor
+        ./gradlew customTask
+*/
+open class CustomTask @javax.inject.Inject constructor(
+    private val message: String,
+    private val number: Int
+) : DefaultTask()
+// TODO: not work
+// tasks.register<CustomTask>("customTask", "wow", 42) {
+//    println("${it.message}")
+// }
+
+
+/*
+    Adding dependencies to a task
+        ./gradlew dependX
+        ./gradlew dependLib
+*/
+// using task object
+val dependX by tasks.registering {
+    doLast {
+        println("dependX")
+    }
+}
+val dependY by tasks.registering {
+    doLast {
+        println("dependY")
+    }
+}
+dependX {
+    dependsOn(dependY)
+}
+
+// using a Gradle Provider
+tasks.register("dependLib") {
+    dependsOn(provider {
+        tasks.filter { task -> task.name.startsWith("lib") }
+    })
+    doLast {
+        println("dependLib")
+    }
+}
+tasks.register("lib1") {
+    doLast {
+        println("lib1")
+    }
+}
+tasks.register("lib2") {
+    doLast {
+        println("lib2")
+    }
+}
+tasks.register("notALib") {
+    // Always print when executing dependLib
+    // since it's printed on configuration
+    println("Always print when scaning")
+    doLast {
+        println("notALib")
+    }
+}
+
+
+/*
+    Ordering tasks
+        TODO
+*/
---
+++ productivity/build-tool/gradle/plugin/README.md
@@ -0,0 +1,5 @@
+# Gradle plugin
+
+## References
+
+https://gradle.org/guides/?q=Plugin%20Development
