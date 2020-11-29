a4d4b496395b03f4edd6467a7f2e4cd8a50e3ae6
Taeik Lim <sibera21@gmail.com>
2020-11-30 02:17:08 +0900

    update
    Signed-off-by: Taeik Lim <sibera21@gmail.com>

--- computer-science-theory/data-structure-and-algorithm/new/recursion/README.md
+++ computer-science-theory/data-structure-and-algorithm/new/recursion/README.md
@@ -52,5 +52,47 @@ int fastSum(const int n) {
 }
 ```
 
+## Tail Recursion
 
+Tail recursion is a recursion where the recursive call is the final instruction in the recursion function.
 
+And there should be only one recursive call in the function.
+
+이렇게 하면 재귀를 할 때 이렇게 하면 할당된 stack을 재사용할 수 있어서 call stack chain이 생기지 않음
+
+```java
+public class Main {
+  private static int helper_non_tail_recursion(int start, int [] ls) {
+    if (start >= ls.length) {
+      return 0;
+    }
+    // not a tail recursion because it does some computation after the recursive call returned.
+    return ls[start] + helper_non_tail_recursion(start+1, ls);
+  }
+
+  public static int sum_non_tail_recursion(int [] ls) {
+    if (ls == null || ls.length == 0) {
+      return 0;
+    }
+    return helper_non_tail_recursion(0, ls);
+  }
+
+  //---------------------------------------------
+
+  // 여기서 start, ls, acc는 한번 할당된 stack에 있는것을 그대로 쓰면 됨
+  private static int helper_tail_recursion(int start, int [] ls, int acc) {
+    if (start >= ls.length) {
+      return acc;
+    }
+    // this is a tail recursion because the final instruction is the recursive call.
+    return helper_tail_recursion(start+1, ls, acc+ls[start]);
+  }
+    
+  public static int sum_tail_recursion(int [] ls) {
+    if (ls == null || ls.length == 0) {
+      return 0;
+    }
+    return helper_tail_recursion(0, ls, 0);
+  }
+}
+```
---
+++ computer-science-theory/data-structure-and-algorithm/new/recursion/SearchInBinarySearchTree.java
@@ -0,0 +1,92 @@
+/*
+ * Descryption
+ *
+ * Given the root node of a binary search tree (BST) and a value.
+ * You need to find the node in the BST that the node's value equals the given value.
+ * Return the subtree rooted with that node. If such node doesn't exist, you should return NULL.
+ * 
+ * For example, 
+ * 
+ * Given the tree:
+ *         4
+ *        / \
+ *       2   7
+ *      / \
+ *     1   3
+ * 
+ * And the value to search: 2
+ * 
+ * You should return this subtree:
+ * 
+ *       2     
+ *      / \   
+ *     1   3
+ * 
+ * In the example above, if we want to search the value 5, since there is no node with value 5, we should return NULL.
+ * 
+ * Note that an empty tree is represented by NULL, therefore you would see the expected output (serialized tree format) as [], not null.
+ *
+ * https://leetcode.com/problems/search-in-a-binary-search-tree/
+ *
+ *
+ * Approach & Proof 
+ *
+ * Base case
+ *   f(null, val) = null
+ *   f(root, val) = root                if root.val == val
+ *
+ * Recursive
+ *   f(root, val) = f(root.left, val)   if root.val > val
+ *   f(root, val) = f(root.right, val)  if root.val < val
+ *
+ *
+ * Complexity
+ *
+ *  - Time  : O(# of node)
+ *  - Space : none
+ *
+ *
+ * Review
+ *
+ * 그냥도 풀 수 있는데 점화식을 세우니까 낫네
+ *
+ *
+ */
+class SearchInBinarySearchTree {
+  public TreeNode searchBST(TreeNode root, int val) {
+    if (root == null) {
+      return null;
+    }
+    
+    if (root.val == val) {
+      return root;
+    } else if (root.val > val) {
+      return searchBST(root.left, val);
+    } else { // root.val < val
+      return searchBST(root.right, val);
+    }
+  }
+
+  public static void main(final String[] args) {
+    final Object[][] parameters = new Object[][] {
+      {
+        TreeNode.of(4, 2, 7, 1, 3),
+        TreeNode.of(2, 1, 3)
+      },
+      {
+        TreeNode.of(2, 1, 3),
+        null
+      },
+    };
+    final SearchInBinarySearchTree solution = new SearchInBinarySearchTree();
+    for (final Object[] parameter : parameters) {
+      final TreeNode input = (TreeNode) parameter[0];
+      final TreeNode expected = (TreeNode) parameter[1];
+      final TreeNode actual = solution.searchBST(input);
+      if (actual.equals(expected)) {
+        throw new IllegalStateException("Expected: " + expected +
+            ", but was: " + actual);
+      }
+    }
+  }
+}
